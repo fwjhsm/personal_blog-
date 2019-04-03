0a66a158855e4aed053d625e77925aff3a489f49
fwjhsm <fengjie_w1@163.com>
2019-04-03 17:04:19 +0800

    创建首页视图

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,'blog/index.html',context={
+        'title':'我的博客主页',
+        'welcome':'欢迎访问我的博客首页'
+    })
