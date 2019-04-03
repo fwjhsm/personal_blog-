4f27b599179e2b13bc44b4d6ff1fc97a42893055
fwjhsm <fengjie_w1@163.com>
2019-04-03 16:24:20 +0800

    创建数据库表

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    # 分类
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    """标签"""
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    """文章"""
+    # 标题
+    title = models.CharField(max_length=70)
+    # 正文
+    body = models.TextField()
+    # 创建时间
+    created_time = models.DateField()
+    # 最后一次修改时间
+    modified_time = models.DateField()
+
+    # 文章摘要
+    excerpt = models.CharField(max_length=200,blank=True)
+
+    # 分类
+    category = models.ForeignKey(Category)
+    # 标签
+    tags = models.ManyToManyField(Tag,blank=True)
+
+    # 文章作者
+    author = models.ForeignKey(User)
